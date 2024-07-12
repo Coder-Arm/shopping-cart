86e61cec7911a2587e192fd56cc29f25327197cd
Ankit <anuastro002@gmail.com>
2024-07-12 13:22:09 +0530

    added search feature

--- src/page/HomePage.jsx
+++ src/page/HomePage.jsx
@@ -8,13 +8,18 @@ import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
    const productsState = useSelector(state => state.products);
-   const carState = useSelector(state => state.cart);
+   const [data,setData] = useState(productsState);
+   const [term,setTerm] = useState('');
+   const cartState = useSelector(state => state.cart);
    console.log('productsState',productsState)
    const dispatch = useDispatch()
   const navigate = useNavigate();
 
     useEffect(() => {
-    
+     if(productsState.length > 0){
+       setData(productsState);
+     }
+     else{
       async function fetchData(){
         const response = await axios.get('https://dummyjson.com/products')
         const products = response.data.products;
@@ -22,9 +27,15 @@ const HomePage = () => {
         dispatch(manageProducts(products));
       } 
       fetchData()
-    
+     }
     },[])
-  
+   
+    useEffect(() => {
+      const filteredData = productsState.filter(item => item.title.toLowerCase().includes(term.toLowerCase().trim()));
+      setData(filteredData)
+    },[term])
+   
+
   return (
     <>
     <header className="flex justify-between p-4 border-2 border-b-zinc-800 ">
@@ -32,19 +43,19 @@ const HomePage = () => {
         <button className="bg-orange-500 pt-2 pb-2 pl-4 pr-4 rounded-md relative"
         onClick={() => navigate('/cart')}
         ><ShoppingCartIcon sx={{color : 'white'}}/>
-        <div className="flex items-center p-2 bg-orange-500 rounded-full absolute top-[-10px] right-[-5px]">
-        <span className="text-white text-xs">{carState.length}</span>
+        <div className="flex items-center p-2 rounded-full absolute top-[-10px] right-[-2px]">
+        <span className="text-white">{cartState.length}</span>
         </div>
         </button>
     </header>
     <main className="mt-8">
         <div className="flex justify-center">
-        <input placeholder="Search product" className="w-[30%] p-2 border-2 border-zinc-800 rounded-md"/>
+        <input placeholder="Search product" className="w-[30%] p-2 border-2 border-zinc-800 rounded-md" value={term} onChange={(e) => setTerm(e.target.value)}/>
         </div>
       {
-        productsState && <div className="grid grid-cols-4 mt-8 gap-8 p-2">
+        data && <div className="grid grid-cols-4 mt-8 gap-8 p-2">
           {
-            productsState.map((item) => {
+            data.map((item) => {
                 return <ProductCard key={item.id} id={item.id} inCart={false}  title={item.title} thumbnail={item.thumbnail} price={item.price} rating={item.rating}/>
             })
           }
